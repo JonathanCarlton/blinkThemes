c9a2964f082ee3a563cf5ebd75047f6aef6b54ed
ThatJonnyC <46948995+ThatJonnyC@users.noreply.github.com>
2021-12-28 19:44:53 -0600

    Update oneHalfDark.js
    Setting new cursor color

--- oneHalfDark.js
+++ oneHalfDark.js
@@ -1,4 +1,4 @@
 t.prefs_.set('color-palette-overrides',["#282c34", "#e06c75", "#98c379", "#e5c07b", "#61afef", "#c678dd", "#56b6c2", "#dcdfe4", "#282c34", "#e06c75", "#98c379", "#e5c07b", "#61afef", "#c678dd", "#56b6c2", "#dcdfe4"]);
 t.prefs_.set('foreground-color', "#dcdfe4");
 t.prefs_.set('background-color', "#282c34");
-t.prefs_.set('cursor-color', 'rgba(49,54,64,0.5)');
+t.prefs_.set('cursor-color', 'rgba(71,78,93,0.5)');
